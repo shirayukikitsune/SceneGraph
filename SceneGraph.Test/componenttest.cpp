4bf437d2217247fc8b712c2d21c3006e4bf3de6e
shirayukikitsune <shirayukikitsune@gmail.com>
2015-08-19 05:58:42 -0300

    TDD :)

---
+++ SceneGraph.Test/componenttest.cpp
@@ -0,0 +1,68 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../SceneGraph/Component.h"
+#include "../SceneGraph/Node.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace sg = kitsune::scenegraph;
+
+namespace SceneGraphTest
+{		
+
+	class InvalidComponent
+		: public sg::Component
+	{
+		KIT_SG_COMPONENT(SceneGraphTest::InvalidComponent);
+
+	public:
+		InvalidComponent(std::shared_ptr<sg::Node> Node) : Component(Node) {}
+		virtual ~InvalidComponent() {}
+	};
+
+	TEST_CLASS(ComponentTest)
+	{
+	public:
+		
+		TEST_METHOD(NodeHasComponent)
+		{
+			std::shared_ptr<kitsune::scenegraph::Node> Node(new kitsune::scenegraph::Node);
+			sg::Component *Component = new sg::Component(Node);
+
+			Node->addComponent(Component);
+
+			Assert::IsTrue(Node->hasComponent<sg::Component>(), L"Component not found");
+		}
+
+		TEST_METHOD(NodeDoesNotHaveComponent)
+		{
+			std::shared_ptr<kitsune::scenegraph::Node> Node(new kitsune::scenegraph::Node);
+			sg::Component *Component = new sg::Component(Node);
+
+			Node->addComponent(Component);
+
+			Assert::IsFalse(Node->hasComponent<InvalidComponent>(), L"Component found");
+		}
+
+		TEST_METHOD(NodeGetComponent)
+		{
+			std::shared_ptr<kitsune::scenegraph::Node> Node(new kitsune::scenegraph::Node);
+			sg::Component *Component = new sg::Component(Node);
+
+			Node->addComponent(Component);
+
+			Assert::IsNotNull(Node->getComponent<sg::Component>(), L"Component not found");
+		}
+
+		TEST_METHOD(NodeGetInvalidComponent)
+		{
+			std::shared_ptr<kitsune::scenegraph::Node> Node(new kitsune::scenegraph::Node);
+			sg::Component *Component = new sg::Component(Node);
+
+			Node->addComponent(Component);
+
+			Assert::IsNull(Node->getComponent<InvalidComponent>(), L"Component found");
+		}
+
+	};
+}
